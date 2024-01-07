edb711dd8013aeef858ff09ebf86b6f9058756b9
comdyax <cody24@tuta.io>
2024-01-07 23:02:01 +0100

    Exc 5.14

--- src/components/Blog.test.js
+++ src/components/Blog.test.js
@@ -2,13 +2,18 @@ import React from 'react'
 import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import Blog from './Blog'
+import userEvent from '@testing-library/user-event'
+import JSDOMEnvironment from 'jest-environment-jsdom'
 
+let user = null
+let blog = null
+let container = null
 
-test('<Blog /> renders only title and author by default', async () => {
-    const user = {
+beforeEach(() => {
+    user = {
         username: 'max'
     }
-    const blog = {
+    blog = {
         title: 'its a me',
         author: 'mario',
         url: 'mariocart.luigi',
@@ -16,12 +21,27 @@ test('<Blog /> renders only title and author by default', async () => {
             username: 'max'
         }
     }
+    container = render(<Blog blog={blog} user={user} reloadBlogs={jest.fn()} />).container
+})
 
-    const { container } = render(<Blog blog={blog} user={user} reloadBlogs={null} />)
 
+test('<Blog /> renders only title and author by default', async () => {
     const divDefault = container.querySelector('.defaultValues')
     const divDetails = container.querySelector('.detailValues')
 
     expect(divDefault).not.toHaveStyle('display: none')
     expect(divDetails).toHaveStyle('display: none')
+})
+
+test('<Blog /> details are shown when show-details-button is clicked', async () => {
+    const client = userEvent.setup()
+    const button = screen.getByText('show details')
+
+    await client.click(button)
+
+    const divDefault = container.querySelector('.defaultValues')
+    const divDetails = container.querySelector('.detailValues')
+
+    expect(divDefault).not.toHaveStyle('display: none')
+    expect(divDetails).not.toHaveStyle('display: none')
 })
